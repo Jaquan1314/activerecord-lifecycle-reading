89a0febdd2d934126b29e14d60404cc3f25c7477
Jaquan1314 <javaughngrant@gmail.com>
2020-10-28 18:15:57 -0400

    done

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,18 +12,18 @@ class PostsController < ApplicationController
 	end
 
 	def create
-	  @post = Post.new(params.require(:post).permit(:title, :description))
+		@post = Post.new(params.require(:post).permit(:title, :description))
     if @post.save
-      redirect_to post_path(@post)
+		redirect_to post_path(@post)
     else
-      render :new
+		render :new
     end
 	end
 
 	def update
-	  @post = Post.find(params[:id])
-	  @post.update(params.require(:post).permit(:title, :description, :post_status, :author_id))
-	  redirect_to post_path(@post)
+		@post = Post.find(params[:id])
+		@post.update(params.require(:post).permit(:title, :description, :post_status, :author_id))
+		redirect_to post_path(@post)
 	end
 
 	def edit
